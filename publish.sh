fd7cf23d4f9ed69ebaa5ed65c7634e1f00f590d0
Kimfucious <kim.f@abts.com.hk>
2018-05-23 23:20:54 +0700

    try netlify publish again

--- publish.sh
+++ publish.sh
@@ -5,9 +5,9 @@ set -e
 
 # https://gist.github.com/cjus/1047794
 echo 'Retrieving latest deploy...'
-url=`curl -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" https://api.netlify.com/api/v1/sites/vibrant-yonath-a27de1/deploys`
+url=`curl -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" https://api.netlify.com/api/v1/sites/kbj.abts.io/deploys`
 temp=`echo $url | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w -m 1 'id'`
 
 # https://www.netlify.com/docs/api/#deploys
 echo "Publishing build ${temp##*|}..."
-curl -X POST -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" -d "{}" "https://api.netlify.com/api/v1/sites/vibrant-yonath-a27de1/deploys/${temp##*|}/restore"
+curl -X POST -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" -d "{}" "https://api.netlify.com/api/v1/sites/kbj.abts.io/deploys/${temp##*|}/restore"
